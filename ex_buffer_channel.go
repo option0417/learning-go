c205b6d816111f634753bfd34c67a37732c74710
WeiDe Su <weide_su@mitake.com.tw>
2019-11-14 18:31:08 +0800

    Add example for "Buffered Channels"

---
+++ ex_buffer_channel.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "time"
+
+func put(c chan int) {
+	for i := 0; i < 3; i++ {
+		fmt.Printf("Put\n")
+		c <- i
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func get(c chan int) {
+	for i := 0; i < 3; i++ {
+		fmt.Printf("Get\n")
+		fmt.Printf("%v\n", <-c)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func main() {
+	ch := make(chan int, 2)
+	go put(ch)
+	go get(ch)
+		time.Sleep(1000 * time.Millisecond)
+}
