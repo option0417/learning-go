c8df1db684c8cad095191e0446c227871ac32d80
Wei De Su <weide_su@mitake.com.tw>
2023-01-11 17:29:15 +0800

    Add stack prototype

--- ds/stack.go
+++ ds/stack.go
@@ -5,12 +5,11 @@ import "fmt"
 type MyStack struct {
 	capacity int
 	currSize int
-
-	entries []Entry
+	entries  []Entry
 }
 
 type Entry struct {
-	val int
+	Val int
 }
 
 func BuildMyStack(capacity int) *MyStack {
@@ -19,12 +18,35 @@ func BuildMyStack(capacity int) *MyStack {
 	return &myStack
 }
 
-func (s MyStack) push(entry Entry) bool {
+func (s *MyStack) Push(entry Entry) bool {
 	fmt.Printf("Invoke push\n")
-	return false
+
+	if !s.isFull() {
+		s.entries[s.currSize] = entry
+		s.currSize++
+		return true
+	} else {
+		return false
+	}
 }
 
-func (s MyStack) pop() bool {
+func (s *MyStack) Pop() Entry {
 	fmt.Printf("Invoke pop\n")
-	return false
+
+	var rtnEntry Entry
+
+	if !s.isEmpty() {
+		s.currSize--
+		rtnEntry = s.entries[s.currSize]
+	}
+
+	return rtnEntry
+}
+
+func (s *MyStack) isFull() bool {
+	return s.capacity == s.currSize
+}
+
+func (s *MyStack) isEmpty() bool {
+	return s.currSize == 0
 }
